b48cabc616b6d83df75e67ee90e33192c804c3bc
leandro160320 <148804278+leandro160320@users.noreply.github.com>
2024-04-10 01:29:57 +0000

    fix: Titulo pagina inicial

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <h1 className="text-center">Página Inicial</h1>
+   <h1 className="text-center">Página Inicial 2</h1>
   );
 }
